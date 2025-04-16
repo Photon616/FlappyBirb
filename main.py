42152f5dd7938e1efc7b9b5ebc2687c737d25f2b
Photon616 <113816102+Photon616@users.noreply.github.com>
2025-04-16 18:31:48 +0900

    screen size edited

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 1024
-screen_height = 1024
+screen_width = 800
+screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("test")
 
 clock = pygame.time.Clock()
-pretendard_black = pygame.font.Font("Assets/Fonts/Pretendard-Black.ttf", 128)
+pretendard_black = pygame.font.Font("Assets/Fonts/Pretendard-Black.ttf", 64)
 
 # image load
 # kabosu_img = pygame.image.load("Assets/kabosu_highres.jpg")
 kabosu_img = pygame.image.load("Assets/Textures/monotoneChecker1k.png")
-kabosu_img = pygame.transform.scale(kabosu_img, (64, 64))
+kabosu_img = pygame.transform.scale(kabosu_img, (48, 48))
 
 checkerboard_img = pygame.image.load("Assets/Textures/uvChecker1k.png")
 checkerboard_img = pygame.transform.scale(checkerboard_img, (1024, 1024))
@@ -24,19 +24,20 @@ monoCheck_img = pygame.image.load("Assets/Textures/monotoneChecker1k.png")
 monoCheck_img = pygame.transform.scale(monoCheck_img, (1024, 1024))
 
 obstacle_checkerboard_img = pygame.image.load("Assets/Textures/uvChecker1k.png")
-obstacle_checkerboard_img = pygame.transform.scale(obstacle_checkerboard_img, (128, 1024))
+obstacle_checkerboard_img = pygame.transform.scale(obstacle_checkerboard_img, (64, 512))
 obstacle_monoCheck_img = pygame.image.load("Assets/Textures/monotoneChecker1k.png")
-obstacle_monoCheck_img = pygame.transform.scale(obstacle_monoCheck_img, (128, 1024))
+obstacle_monoCheck_img = pygame.transform.scale(obstacle_monoCheck_img, (64, 512))
 
 # variables
 player_speed = 5
-player_x_offset = 192
-player_y_offset = 256  
-player_y_vel = 11.0
+player_x_offset = 200
+player_y_offset = 0  
+player_y_vel = 0.0
 terminal_velocity = 100
-gap_range = 256
-gap_height = random.randint(374, 1024 - 374)
+gap_range = 160
+gap_height = random.randint(200, 400)
 score = 0
+can_score = True
 
 #classes
 class Player:
@@ -47,12 +48,12 @@ class Player:
 
     def update(self):
         self.rect.y += self.velocity
-        self.velocity += 0.5
+        self.velocity += 0.3
         if self.velocity > terminal_velocity:
             self.velocity = terminal_velocity
 
     def jump(self):
-        self.velocity = -11 - (score // 5)
+        self.velocity = -7 - (score // 5)
 
     def draw(self, surface):
         surface.blit(self.img, self.rect.topleft)
@@ -61,14 +62,16 @@ class Obstacles:
     def __init__(self, rimg, x, y):
         self.img = rimg
         self.rect = pygame.Rect(x, y, rimg.get_width() * 0.8, rimg.get_height())
-        self.velocity = 8
+        self.velocity = 6
 
     def update(self):
         self.rect.x -= self.velocity
-        if self.rect.x < (-1 * screen_width) + 512:
+        if self.rect.x < (-1 * screen_width) + 400:
             self.rect.x = screen_width
             global gap_height 
-            gap_height = random.randint(374, 1024 - 374)
+            global can_score
+            gap_height = random.randint(150, 450)
+            can_score = True
 
     def draw(self, surface):
         surface.blit(self.img, (self.rect.x - self.img.get_width() * 0.1, self.rect.y))
@@ -79,21 +82,6 @@ class Obstacles:
         else:
             self.rect.y = (height) + (gap_range / 2)
         # self.rect.y = height + ((gap_range / 2) * multiplyer)
-
-class parallax_bg:
-    def __init__(self, rimg, x, y, velFac):
-        self.img = rimg
-        self.rect = pygame.Rect(x, y, rimg.get_width(), rimg.get_height())
-        self.velocity = 8
-        self.factor = velFac
-
-    def update(self):
-        self.rect.x -= self.velocity * self.factor
-        if self.rect.x < -1024:
-            self.rect.x = screen_width
-
-    def draw(self, surface):
-        surface.blit(self.img, self.rect.topleft)
 
 def start_screen():
     running = True
@@ -114,6 +102,7 @@ def gameOverScreen():
 
 def in_game():
     global score
+    global can_score
     global pretendard_black
 
     pl = Player(kabosu_img, 192, 0) # the player
@@ -121,6 +110,7 @@ def in_game():
     obsAbove = Obstacles(obstacle_monoCheck_img, 1024, 0) # the obstacle coming above
 
     score = 0 # reset score
+    can_score = True
 
     running = True
     while running:
@@ -139,15 +129,17 @@ def in_game():
             return "gameover"
 
         # score player
-        if pl.rect.x == obsAbove.rect.x:
+        if pl.rect.x >= obsAbove.rect.x and can_score:
             score += 1
+            can_score = False
         
         # show score
         score_text = pretendard_black.render(str(score), True, (255, 255, 255))
-        score_text_rect = score_text.get_rect(center=(512, 512))
+        score_text_rect = score_text.get_rect(center=(400, 300))
 
         # draw elements
         # background
+        # screen.blit(checkerboard_img, (0, 0))
         screen.blit(score_text, score_text_rect)
 
         # other elements
