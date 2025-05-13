6378f0e720074edc5b61216fb9946325fc6edf0c
Photon616 <113816102+Photon616@users.noreply.github.com>
2025-05-13 13:43:19 +0900

    asdf

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
+import time
 import pygame
 from pygame.locals import *
 
 pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("Assets/Audios/glitchrun.wav")
+pygame.mixer.music.set_volume(0.9)
 
 screen_width = 640
 screen_height = 480
@@ -12,6 +16,7 @@ pygame.display.set_caption("test")
 
 clock = pygame.time.Clock()
 pretendard_black = pygame.font.Font("Assets/Fonts/Pretendard-Black.ttf", 64)
+pretendard = pygame.font.Font("Assets/Fonts/Pretendard-Regular.ttf", 32)
 
 # image load
 # kabosu_img = pygame.image.load("Assets/kabosu_highres.jpg")
@@ -29,6 +34,12 @@ obstacle_monoCheck_img = pygame.image.load("Assets/Textures/monotoneChecker1k.pn
 obstacle_monoCheck_img = pygame.transform.scale(obstacle_monoCheck_img, (64, 512))
 
 bg_img = pygame.image.load("Assets/Textures/placeholder_640x480.png")
+
+player_img = pygame.image.load("Assets/Textures/player_box.png")
+
+# audio load
+death_sound = pygame.mixer.Sound('Assets/Audios/death_glitch.wav')
+death_sound.set_volume(0.6)
 
 # variables
 player_speed = 5
@@ -57,7 +68,7 @@ class Player:
             self.velocity = terminal_velocity
 
     def jump(self):
-        self.velocity = -7 - (score // 10)
+        self.velocity = -7 - (score // 7)
 
     def draw(self, surface):
         surface.blit(self.img, self.rect.topleft)
@@ -88,31 +99,89 @@ class Obstacles:
         # self.rect.y = height + ((gap_range / 2) * multiplyer)
 
 def start_screen():
+    global pretendard
+    global pretendard_black
+
     running = True
     keys = pygame.key.get_pressed()
 
     while running:
-        if True:
-            return "game"
+        # initiallize the screen
+        screen.fill((0, 0, 0))
+        clock.tick(60)
+        
+        title_text = pretendard_black.render("Flappy Birb", True, (255, 255, 255))
+        title_text_rect = title_text.get_rect(center=(screen_width / 2, (screen_height / 2) - 150))
+        info_text = pretendard.render("SPACE를 눌러 점프", True, (255, 255, 255))
+        info_text_rect = info_text.get_rect(center=(screen_width / 2, screen_height / 2))
 
+        # screen.blit(bg_img, (0, 0))
+        screen.blit(title_text, title_text_rect)
+        screen.blit(info_text, info_text_rect)
+        
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return "game"
             if event.type == pygame.QUIT:
                 running = False
+        
+        pygame.display.update()
 
     # to be finished
 
 def gameOverScreen():
-    # to be finished
-    return "start"
+    global player_x_offset
+    global player_y_offset
+
+    # 클리어 음악이나 효과음이 있다면 여기서 재생 가능
+    # pygame.mixer.music.stop()
+    death_sound.play()
+
+    global pretendard
+    global pretendard_black
+
+    running = True
+    keys = pygame.key.get_pressed() 
+
+    while running:
+        # initiallize the screen
+        screen.fill((0, 0, 0))
+        clock.tick(60)
+        
+        title_text = pretendard_black.render("Game Over", True, (255, 255, 255))
+        title_text_rect = title_text.get_rect(center=(screen_width / 2, (screen_height / 2) - 150))
+        info_text = pretendard.render("메인화면으로(SPACE)", True, (255, 255, 255))
+        info_text_rect = info_text.get_rect(center=(screen_width / 2, screen_height / 2))
+
+        # screen.blit(bg_img, (0, 0))
+        screen.blit(title_text, title_text_rect)
+        screen.blit(info_text, info_text_rect)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return "start"
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pygame.display.update()
+    
+    # pygame.time.wait(1000)
 
 def in_game():
     global score
     global can_score
     global pretendard_black
 
-    pl = Player(kabosu_img, 192, 0) # the player
+    pl = Player(player_img, 192, 0) # the player
     obsBelow = Obstacles(obstacle_checkerboard_img, 1024, 0) # the obstacle coming below
     obsAbove = Obstacles(obstacle_monoCheck_img, 1024, 0) # the obstacle coming above
+
+    # # 음악 재생 (이미 mixer.init()이 한 번 되어 있다고 가정)
+    # pygame.mixer.music.set_volume(0.9)
+    # pygame.mixer.music.load("Assets/Audios/glitchrun.wav")
+    # pygame.mixer.music.play(-1)  # 반복 재생
 
     score = 0 # reset score
     can_score = True
@@ -144,7 +213,7 @@ def in_game():
 
         # draw elements
         # background
-        screen.blit(bg_img, (0, 0))
+        # screen.blit(bg_img, (0, 0))
         screen.blit(score_text, score_text_rect)
 
         # other elements
@@ -184,6 +253,11 @@ def main():
 
     pygame.quit()
     return
+
+# 음악 재생 (이미 mixer.init()이 한 번 되어 있다고 가정)
+pygame.mixer.music.set_volume(0.9)
+pygame.mixer.music.load("Assets/Audios/glitchrun.wav")
+pygame.mixer.music.play(-1)  # 반복 재생
 
 #initial start
 main()
