98b13a4c6278cfc38d985314e39c672b90a6f261
Photon616 <113816102+Photon616@users.noreply.github.com>
2025-08-26 21:29:28 +0900

    Update main.py

--- hundred_relief_plan/main.py
+++ hundred_relief_plan/main.py
@@ -24,42 +24,45 @@ bullet_img = pygame.image.load("hundred_relief_plan/Assets/Textures/kabosu_highr
 bullet_img = pygame.transform.scale(bullet_img, (50, 10))
 cue_img = pygame.image.load("hundred_relief_plan/Assets/Textures/player_box.png") 
 cue_img = pygame.transform.scale(cue_img, (32, scrnH))
+#effects
 explosion_img = pygame.image.load("hundred_relief_plan/Assets/Textures/uvChecker1k.png") 
-explosion_img = pygame.transform.scale(explosion_img, (96, 96))
-
+explosion_img = pygame.transform.scale(explosion_img, (128, 128))
+package_img = pygame.image.load("hundred_relief_plan/Assets/Textures/uvChecker1k.png") 
+package_img = pygame.transform.scale(package_img, (48, 48))
+#bg
 start_screen_background = pygame.image.load("hundred_relief_plan/Assets/Textures/placeholder_640x480.png")
 gameover_screen_background = pygame.image.load("hundred_relief_plan/Assets/Textures/placeholder_800x600.png")
 
 # classes
 class Player:
-    def __init__(self, rimg, x, y, y_vel_limit, rect_x, rect_y, rect_size_x, rect_size_y):
+    def __init__(self, rimg, x, y, y_spd_limit, rect_x, rect_y, rect_size_x, rect_size_y):
         self.rimg = rimg
         self.x = x
         self.y = y
-        self.y_vel = 0
-        self.y_vel_limit = y_vel_limit
+        self.y_spd = 0
+        self.y_spd_limit = y_spd_limit
         self.rect = pygame.Rect(rect_x, rect_y, rect_size_x, rect_size_y)
     
     def update(self, fall_speed):
-        self.y_vel += fall_speed 
+        self.y_spd += fall_speed 
 
-        if self.y_vel > self.y_vel_limit:
-            self.y_vel = self.y_vel_limit
+        if self.y_spd > self.y_spd_limit:
+            self.y_spd = self.y_spd_limit
 
-        self.y += self.y_vel # adds on y coordinate to go down
+        self.y += self.y_spd # adds on y coordinate to go down
 
         # checks if the player is colliding with the top and bottom of the screen
         if (self.y + self.rect.height) > scrnH:
             self.y = scrnH - self.rect.height
-            self.y_vel = 0
+            self.y_spd = 0
         elif self.y < 0:
             self.y = 0
-            self.y_vel = 0
+            self.y_spd = 0
 
         self.rect.y = self.y
     
     def jump(self, jump_speed):
-        self.y_vel = -jump_speed
+        self.y_spd = -jump_speed
     
     def draw(self):
         screen.blit(self.rimg, (self.x, self.y))
@@ -108,9 +111,36 @@ class Explosion(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect(center = pos)
         self.timer = 15  # frames until disappears
+
     def update(self):
         self.timer -= 1
         if self.timer <= 0:
+            self.kill()
+
+class Relief_Package(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, y_spd, x_spd_limit):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(center = (x, y))
+        self.x = x
+        self.y = y
+        self.x_spd = 0
+        self.y_spd = y_spd
+        self.x_spd_limit = x_spd_limit
+    
+    def update(self, fall_speed):
+        self.x_spd += fall_speed 
+
+        if self.x_spd > self.x_spd_limit:
+            self.x_spd = self.x_spd_limit
+
+        self.y += self.y_spd # adds on y coordinate to go down
+        self.x -= self.x_spd
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        if self.rect.top >= scrnH or self.rect.right <= 0:
             self.kill()
 
 def start_screen():
@@ -137,14 +167,20 @@ def in_game():
     enemies = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
     effects = pygame.sprite.Group()
+    relief_packages = pygame.sprite.Group()
     fall_speed = 0.3
     jump_speed = 8.0 
+    # enemy
     enemy_duration = 4 # in sec. division will be done after
     enemy_time = (enemy_duration - 1) * 60
     enemy_speed = 4
+    # weapon
     weapon_duration = .5 # in sec
     weapon_time = weapon_duration * 60
     bullet_speed = 10
+    # supply package
+    supply_duration = 5 # in sec
+    supply_time = 0
 
     score = 0
 
@@ -157,6 +193,14 @@ def in_game():
             enemies.add(Obstacle(enemy_img, 680, enemy_speed, 110, 340, pl.rect.right, cue_img))
             # print("really added")
             enemy_time = 0 # initialize time to prevent overflow/save memory
+        
+        if supply_time / 60 >= supply_duration: # similar to enemies
+            if pl.y_spd < 0:
+                package_speed = -1
+            else:
+                package_speed = pl.y_spd * .5
+            relief_packages.add(Relief_Package(package_img, pl.rect.centerx - 24, pl.rect.centery - 24, package_speed + 2, 50))
+            supply_time = 0
 
         # keys = pygame.key.get_pressed()
         # if keys[pygame.K_f] and weapon_time / 60 >= weapon_time:
@@ -168,6 +212,7 @@ def in_game():
         bullets.update()
         effects.update()
         enemies.update()
+        relief_packages.update(0.2)
         for target_enemy in enemies:
             if target_enemy.is_over:
                 return "gameover"
@@ -181,12 +226,14 @@ def in_game():
 
         bullets.draw(screen)
         enemies.draw(screen) 
+        relief_packages.draw(screen)
         pl.draw() # blit rimg on the screen
         effects.draw(screen)
 
         pygame.display.update() # update the screen
         enemy_time += 1
         weapon_time += 1
+        supply_time += 1
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -194,7 +241,7 @@ def in_game():
                     pl.jump(jump_speed)
                 if event.key == pygame.K_f and weapon_time / 60 >= weapon_duration:
                     # print("bullet shot")
-                    bullets.add(Bullet(bullet_img, pl.x, pl.y, bullet_speed, scrnW))
+                    bullets.add(Bullet(bullet_img, pl.x, pl.rect.center[1] - 5, bullet_speed, scrnW))
                     weapon_time = 0
                 if event.key == pygame.K_ESCAPE:
                     return "start"
